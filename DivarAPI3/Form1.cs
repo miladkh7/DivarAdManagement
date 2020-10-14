9077c816926783328e5d37000399007e5c7c123f
milad <milad_khaleghi@Live.com>
2020-10-14 21:16:23 +0330

    first commit

---
+++ DivarAPI3/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DivarAPI3
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkxOTcyODcwNTgiLCJleHAiOjE2MDM5NTY4MTIuOTQ4MDQxLCJ2ZXJpZmllZF90aW1lIjoxNjAyNjYwODEyLjk0ODAyMiwidXNlci10eXBlIjoibWFya2V0cGxhY2UtYnVzaW5lc3MifQ.SaPGMEC21kAcjQKnN7ojp4EGYlqE4hGQDL_bFMEx-Nk";
+
+            //Divar myDivar = new Divar();
+            //List myDivar.GetData(accessToken);
+            //MessageBox.Show(re);
+            //Divar.GetData();
+            //myDivar.GetData();
+        }
+    }
+}
---
+++ DivarAPI3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft;
+using Newtonsoft.Json.Linq;
+using System.Text;
+namespace DivarAPI3
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    class DivarPost
+    {
+        private string _title, _date, _token;
+        public DivarPost(string title,string token)
+        {
+            this._title = title;
+            this._token = token;
+
+        }
+        public override string ToString()
+        {
+            return "_title ,_token".ToString();
+        }
+    }
+
+    class Divar
+    {
+        public static WebClient client;
+        public Divar()
+        {
+            client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            client.Headers[HttpRequestHeader.ContentType] = "application/json";
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; " +
+                      "Windows NT 5.2; .NET CLR 1.0.3705;)");
+            //client.Headers[
+        }
+
+        public List<DivarPost> GetData(string accessToken)
+        {
+            List<DivarPost> posts = new List<DivarPost>();
+
+            string getPostUrl = "https://api.divar.ir/v8/ongoingposts/userposts";
+
+            client.Headers.Add("Authorization", "Bearer " + accessToken);
+
+            string result = client.DownloadString(getPostUrl);
+
+            JObject j = JObject.Parse(result);
+
+            foreach (var item in j["widget_list"])
+            {
+                string newPostTitle = item["data"]["title"].ToString();
+                string newPostToken = item["data"]["token"].ToString();
+
+                DivarPost currentDivarPost = new DivarPost(newPostTitle, newPostToken);
+
+                posts.Add(currentDivarPost);
+
+  
+            }
+
+            return posts;
+
+
+        }
+    }
+
+
+
+}
