948b9ff981cfd9c6ad4cd57abd7f1b21f04feb6d
milad <milad_khaleghi@Live.com>
2020-10-15 10:52:10 +0330

    add delete all

--- DivarAPI3/Program.cs
+++ DivarAPI3/Program.cs
@@ -26,11 +26,22 @@ namespace DivarAPI3
 
     class DivarPost
     {
-        private string _title, _date, _token;
-        public DivarPost(string title,string token)
+        private string _title, _date, _token,_management_token;
+        
+        public string token
+        {
+            get { return _token; }
+        }
+
+        public string managementToken
+        {
+            get { return managementToken; }
+        }
+        public DivarPost(string title, string token,string manageToken)
         {
             this._title = title;
             this._token = token;
+            this._management_token = manageToken;
 
         }
         public override string ToString()
@@ -42,6 +53,10 @@ namespace DivarAPI3
     class Divar
     {
         public static WebClient client;
+        public static string accessToken;
+
+        List<DivarPost> Posts;
+
         public Divar()
         {
             client = new WebClient();
@@ -50,9 +65,22 @@ namespace DivarAPI3
             client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; " +
                       "Windows NT 5.2; .NET CLR 1.0.3705;)");
             //client.Headers[
+            Posts = new List<DivarPost>();
         }
 
-        public List<DivarPost> GetData(string accessToken)
+
+        public void GetPosts()
+        {
+            this.Posts = GetData();
+        }
+
+
+        public static List<DivarPost> GetData()
+        {
+           return GetData(accessToken);
+
+        }
+        public static List<DivarPost> GetData(string accessToken)
         {
             List<DivarPost> posts = new List<DivarPost>();
 
@@ -67,21 +95,64 @@ namespace DivarAPI3
             foreach (var item in j["widget_list"])
             {
                 string newPostTitle = item["data"]["title"].ToString();
-                string newPostToken = item["data"]["token"].ToString();
+                string newPostToken = item["data"]["manage_token"].ToString();
 
-                DivarPost currentDivarPost = new DivarPost(newPostTitle, newPostToken);
+                DivarPost currentDivarPost = new DivarPost(newPostTitle, newPostToken,accessToken);
 
                 posts.Add(currentDivarPost);
 
-  
+
             }
 
             return posts;
 
 
         }
+
+
+        public void AproveAdvertisement(string accessToken, string managmentToken)
+        {
+            string aproveUrl = String.Format("https://api.divar.ir/v8/ongoingposts/{}/claim", managmentToken);
+                
+            client.Headers.Add("Authorization", "Bearer " + accessToken);
+            string result = client.DownloadString(aproveUrl);
+
+        }
+
+
+        public void DeleteAdvertisment(string accessToken,string managmentToken)
+        {
+            string deleteUrl = String.Format("https://api.divar.ir/v8/ongoingposts/{}", managmentToken);
+            string fullUrl = String.Format("{}?answer=&question_tag=note&reason=R", deleteUrl);
+
+
+            client.Headers.Add("Authorization", "Bearer " + accessToken);
+            client.UploadString(fullUrl, "DELETE");
+
+            string result = client.DownloadString(fullUrl);
+        }
+
+
+        public void DeleteAllAdevertisment()
+        {
+            foreach (DivarPost post in this.Posts)
+            {
+                try
+                {
+                    DeleteAdvertisment(post.token, post.managementToken);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }    
+        }
     }
 
-
-
+    public static class Messages
+    {
+        const string ready = "plese select your excel file";
+    }
 }
