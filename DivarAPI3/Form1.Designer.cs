e12b87e0c387c68ba69887a7f05c420437cd5bb4
milad <milad_khaleghi@Live.com>
2020-10-15 10:25:06 +0330

    modify ui

--- DivarAPI3/Form1.Designer.cs
+++ DivarAPI3/Form1.Designer.cs
@@ -31,27 +31,29 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridViewTokens = new System.Windows.Forms.DataGridView();
             this.btnBrowse = new System.Windows.Forms.Button();
             this.rdoApprove = new System.Windows.Forms.RadioButton();
             this.rdoDelete = new System.Windows.Forms.RadioButton();
             this.btnApply = new System.Windows.Forms.Button();
-            this.clmNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.clmToken = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.statusText = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.clmNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmToken = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTokens)).BeginInit();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridViewTokens
             // 
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.dataGridViewTokens.AllowUserToDeleteRows = false;
+            this.dataGridViewTokens.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewTokens.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewTokens.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(65)))));
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
             dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
@@ -59,9 +61,9 @@
             dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTokens.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewTokens.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTokens.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.clmNumber,
             this.clmToken});
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
@@ -71,26 +73,33 @@
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 45);
-            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridViewTokens.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewTokens.Location = new System.Drawing.Point(12, 45);
+            this.dataGridViewTokens.Name = "dataGridViewTokens";
+            this.dataGridViewTokens.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.MenuText;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Vazir", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.Info;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
-            this.dataGridView1.Size = new System.Drawing.Size(288, 352);
-            this.dataGridView1.TabIndex = 3;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridViewTokens.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.dataGridViewTokens.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridViewTokens.Size = new System.Drawing.Size(288, 325);
+            this.dataGridViewTokens.TabIndex = 3;
+            this.dataGridViewTokens.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // btnBrowse
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(240, 12);
+            this.btnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowse.Font = new System.Drawing.Font("Vazir", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnBrowse.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnBrowse.Location = new System.Drawing.Point(12, 12);
             this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(60, 23);
+            this.btnBrowse.Size = new System.Drawing.Size(60, 27);
             this.btnBrowse.TabIndex = 4;
             this.btnBrowse.Text = "انتخاب فایل";
             this.btnBrowse.UseVisualStyleBackColor = true;
@@ -99,9 +108,12 @@
             // rdoApprove
             // 
             this.rdoApprove.AutoSize = true;
-            this.rdoApprove.Location = new System.Drawing.Point(12, 403);
+            this.rdoApprove.Font = new System.Drawing.Font("Vazir", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.rdoApprove.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rdoApprove.Location = new System.Drawing.Point(214, 376);
             this.rdoApprove.Name = "rdoApprove";
-            this.rdoApprove.Size = new System.Drawing.Size(90, 17);
+            this.rdoApprove.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.rdoApprove.Size = new System.Drawing.Size(86, 22);
             this.rdoApprove.TabIndex = 6;
             this.rdoApprove.TabStop = true;
             this.rdoApprove.Text = "تایید آگهی ها";
@@ -110,22 +122,59 @@
             // rdoDelete
             // 
             this.rdoDelete.AutoSize = true;
-            this.rdoDelete.Location = new System.Drawing.Point(12, 426);
+            this.rdoDelete.Font = new System.Drawing.Font("Vazir", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.rdoDelete.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rdoDelete.Location = new System.Drawing.Point(213, 400);
             this.rdoDelete.Name = "rdoDelete";
-            this.rdoDelete.Size = new System.Drawing.Size(89, 17);
+            this.rdoDelete.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.rdoDelete.Size = new System.Drawing.Size(87, 22);
             this.rdoDelete.TabIndex = 7;
             this.rdoDelete.TabStop = true;
             this.rdoDelete.Text = "حذف اگهی ها";
+            this.rdoDelete.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.rdoDelete.UseVisualStyleBackColor = true;
             // 
             // btnApply
             // 
-            this.btnApply.Location = new System.Drawing.Point(214, 403);
+            this.btnApply.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnApply.Font = new System.Drawing.Font("Vazir", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnApply.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnApply.Location = new System.Drawing.Point(12, 376);
             this.btnApply.Name = "btnApply";
             this.btnApply.Size = new System.Drawing.Size(86, 40);
             this.btnApply.TabIndex = 8;
             this.btnApply.Text = "ارسال";
             this.btnApply.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.statusStrip1.Font = new System.Drawing.Font("Vazir", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusText,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 425);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.statusStrip1.Size = new System.Drawing.Size(312, 24);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusText
+            // 
+            this.statusText.Font = new System.Drawing.Font("Vazir", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusText.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.statusText.Name = "statusText";
+            this.statusText.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.statusText.Size = new System.Drawing.Size(161, 19);
+            this.statusText.Text = "Please Select Your Excel File";
+            this.statusText.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 18);
+            this.toolStripProgressBar1.Visible = false;
             // 
             // clmNumber
             // 
@@ -134,53 +183,34 @@
             this.clmNumber.HeaderText = "ردیف1";
             this.clmNumber.MaxInputLength = 100;
             this.clmNumber.Name = "clmNumber";
-            this.clmNumber.Width = 120;
+            this.clmNumber.ReadOnly = true;
+            this.clmNumber.Width = 50;
             // 
             // clmToken
             // 
             this.clmToken.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
             this.clmToken.HeaderText = "ردیف2";
             this.clmToken.Name = "clmToken";
-            this.clmToken.ReadOnly = true;
             this.clmToken.Width = 141;
-            // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.statusText,
-            this.toolStripProgressBar1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 427);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(312, 22);
-            this.statusStrip1.TabIndex = 9;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // statusText
-            // 
-            this.statusText.Name = "statusText";
-            this.statusText.Size = new System.Drawing.Size(152, 17);
-            this.statusText.Text = "Please Select Your Excel File";
-            // 
-            // toolStripProgressBar1
-            // 
-            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
-            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
-            this.toolStripProgressBar1.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
             this.ClientSize = new System.Drawing.Size(312, 449);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.btnApply);
             this.Controls.Add(this.rdoDelete);
             this.Controls.Add(this.rdoApprove);
             this.Controls.Add(this.btnBrowse);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridViewTokens);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DivarApi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTokens)).EndInit();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -190,9 +220,7 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn clmNumber;
-        private System.Windows.Forms.DataGridViewTextBoxColumn clmToken;
+        private System.Windows.Forms.DataGridView dataGridViewTokens;
         private System.Windows.Forms.Button btnBrowse;
         private System.Windows.Forms.RadioButton rdoApprove;
         private System.Windows.Forms.RadioButton rdoDelete;
@@ -200,6 +228,8 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel statusText;
         private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmToken;
     }
 }
 
--- DivarAPI3/Form1.cs
+++ DivarAPI3/Form1.cs
@@ -14,12 +14,27 @@ namespace DivarAPI3
     {
 
 
-
+        MyExcel refrenceDataBase;
+        List<string> tokens;
         public Form1()
         {
             InitializeComponent();
         }
+        /// <summary>
+        /// This method use to add
+        /// </summary>
+        /// <param name="dataGridToFill"></param>
+        /// <param name="myList"></param>
+        public void EnterListInDataGridView(DataGridView dataGridToFill, List<string> myList)
+        {
+            int index = 1;
+            foreach (var item in myList)
+            {
 
+                dataGridToFill.Rows.Add(index, item.ToString());
+                index++;
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             //string accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkxOTcyODcwNTgiLCJleHAiOjE2MDM5NTY4MTIuOTQ4MDQxLCJ2ZXJpZmllZF90aW1lIjoxNjAyNjYwODEyLjk0ODAyMiwidXNlci10eXBlIjoibWFya2V0cGxhY2UtYnVzaW5lc3MifQ.SaPGMEC21kAcjQKnN7ojp4EGYlqE4hGQDL_bFMEx-Nk";
@@ -37,6 +52,22 @@ namespace DivarAPI3
         }
 
         private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            refrenceDataBase = new MyExcel();
+            refrenceDataBase.OpenExcelFileWithDialog();
+            if (refrenceDataBase.isExcelFilePathValid)
+            {
+                statusText.Text = "please Wait";
+                tokens = refrenceDataBase.ReadToken();
+                statusText.Text = "finish";
+                refrenceDataBase.Close();
+                EnterListInDataGridView(dataGridViewTokens, tokens);
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
